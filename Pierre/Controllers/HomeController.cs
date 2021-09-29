a3975a0eeb06d47d1daeb8edf3f29d4272fa8574
Alex Bertotto <ajb5206@gmail.com>
2021-09-29 12:52:05 -0700

    add homecontroller file

---
+++ Pierre/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PierreTracker.Controllers
+{
+	public class HomeController : Controllers
+	{
+		public string Welcome() { return "Welcome To Pierre's!"}
+		
+	}
+}
