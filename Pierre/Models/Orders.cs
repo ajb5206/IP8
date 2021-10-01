b4aafaa80860d45139ec2a400cd5eb0c2470be8e
Alex Bertotto <ajb5206@gmail.com>
2021-10-01 13:10:26 -0700

    add list functionality to Orders.cs

--- Pierre/Models/Orders.cs
+++ Pierre/Models/Orders.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace PierreTracker.Models
 {
 	public class Order
@@ -6,6 +8,21 @@ namespace PierreTracker.Models
 		public string ProductDescription { get; set; }
 		public int Price { get; set; }
 		public string Date { get; set; }
+		private static List<Item> _instances = new List<Item> {};
+
+		public Item(string product, string productDescription, int price, string date)
+		{
+			Product = product;
+			ProductDescription = productDescription;
+			Price = price;
+			Date = date;
+			_instances.Add(this);
+		}
+
+		public static List<Item> GetAll()
+		{
+			return _instances;
+		}
 
 	}
 }
