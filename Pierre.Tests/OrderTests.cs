37925d94bd6e66aa46f68d1400e4b5669d7c4a26
Alex Bertotto <ajb5206@gmail.com>
2021-10-02 14:35:08 -0700

    add code for instantiating constructor

---
+++ Pierre.Tests/OrderTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierreTracker.Tests
+{
+	[TestClass]
+	public class OrderTests : IDisposable
+	{
+		public void Dispose()
+		{
+			Item.ClearAll();
+		}
+
+		[TestMethod]
+		public void OrderConstructor_CreatesInstanceOfOrder_Order()
+		{
+			Order newOrder = new Order("test", "test", 1, "test");
+			Assert.AreEqual(typeof(Order), newOrder.GetType());
+		}
+	}
+}
--- Pierre/Models/Orders.cs
+++ Pierre/Models/Orders.cs
@@ -8,9 +8,9 @@ namespace PierreTracker.Models
 		public string ProductDescription { get; set; }
 		public int Price { get; set; }
 		public string Date { get; set; }
-		private static List<Item> _instances = new List<Item> {};
+		private static List<Order> _instances = new List<Order> {};
 
-		public Item(string product, string productDescription, int price, string date)
+		public Order(string product, string productDescription, int price, string date)
 		{
 			Product = product;
 			ProductDescription = productDescription;
@@ -19,7 +19,12 @@ namespace PierreTracker.Models
 			_instances.Add(this);
 		}
 
-		public static List<Item> GetAll()
+		public static void ClearAll()
+		{
+			_instances.Clear();
+		}
+
+		public static List<Order> GetAll()
 		{
 			return _instances;
 		}
