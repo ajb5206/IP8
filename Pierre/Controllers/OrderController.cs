286cd9ffac08d86ceeb770b4fbb3a4570d96966a
Alex Bertotto <ajb5206@gmail.com>
2021-10-02 23:05:18 -0700

    add updated Order and Vendor controllors following todolist format

--- Pierre/Controllers/OrderController.cs
+++ Pierre/Controllers/OrderController.cs
@@ -13,16 +13,23 @@ namespace PierreTracker.Controllers
 			Vender vendor = Vendor.Find(vendorId);
 			return View(vendor);
 		}
-		
+
 		[HttpGet("/vendors/{vendorId}/orders/{orderId}")]
 		public ActionResult Show(int vendorId, int orderId)
 		{ 
-			Order newOrder = new Order("");
-			newOrder.Product = product;
-			newOrder.ProductDescription = productDescription;
-			newOrder.Price = price;
-			newOrder.Date = date;
-			return View(newOrder); 
+			Order newOrder = Order.Find(orderId);
+			Vendor vendor = Vendor.Find(vendorId);
+			Dictionary<string, object> model = new Dictionary<string, object>();
+			model.Add("order", order);
+			model.Add("vendor", vendor);
+			return View(model); 
+		}
+
+		[HttpPost("/orders/delete")]
+		public ActionResult DeleteAll()
+		{
+			Order.ClearAll();
+			return View();
 		}
 	}
 }
--- Pierre/Controllers/VendorControllor.cs
+++ Pierre/Controllers/VendorControllor.cs
@@ -26,5 +26,16 @@ namespace PierreTracker.Controllers
 			Vendor newVendor = new Vendor(vendorName, description);
 			return RedirectToAction("Index");
 		}
+
+		[HttpGet("/categories/{id}")]
+		public ActionResult Show(int id)
+		{
+			Dictionary<string, object> model = new Dictionary<string, object>();
+			Vendor selectedVendor = Vendor.Find(id);
+			List<Order> vendorOrders = selectedVendor.Orders;
+			model.Add("vendor", selectedVendor);
+			model.Add("orders", vendorOrders);
+			return View(model);
+		}
 	}
 }
