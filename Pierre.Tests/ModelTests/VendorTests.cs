2b379a5dad423e94ddda43907ab35b6aa0612304
Alex Bertotto <ajb5206@gmail.com>
2021-10-03 01:10:05 -0700

    add passing test for returning vendor name

--- Pierre.Tests/ModelTests/VendorTests.cs
+++ Pierre.Tests/ModelTests/VendorTests.cs
@@ -20,5 +20,15 @@ namespace PierreTracker.Tests
 			Vendor newVendor = new Vendor("test vendor", "test description");
 			Assert.AreEqual(typeof(Vendor), newVendor.GetType());
 		}
+
+		[TestMethod]
+		public void GetVendorName_ReturnsVendorName_String()
+		{
+			string vendorName = "Test Vendor";
+			string description = "Test description";
+			Vendor newVendor = new Vendor(vendorName, description);
+			string result = newVendor.VendorName;
+			Assert.AreEqual(vendorName, result);
+		}
 	}
 }
